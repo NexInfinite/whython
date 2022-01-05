944b4b10bf6b8966a61d87f993a7c5c5dd730408
nexinfinite <julianjones663@gmail.com>
2022-01-05 17:23:49 +0000

    Added welcome message

--- whython/shell.py
+++ whython/shell.py
@@ -1,9 +1,12 @@
 import run as whython
+import os
 
 def main():
+    welcome()
     while True:
         text = input("whython > ")
         if text.strip() == "": continue
+        if text.strip() == "exit": print("Goodbye!"); return
         result, error = whython.run("<stdin>", text)
 
         if error: print(error.as_string())
@@ -14,6 +17,13 @@ def main():
                         print(element)
                 except AttributeError:
                     pass
+
+def welcome():
+    print("Welcome to whython v1.3")
+
+    # Get info about saves
+    editing_location = os.path.abspath("editable_/editable.py")
+    print(f"Current save location for editing func/var names is: {editing_location}\n")
 
 if __name__ == "__main__":
     main()
